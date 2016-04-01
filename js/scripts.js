169a50ee9a0c3f658b4a58cd874641cb00f1edeb
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-04-01 15:17:38 -0700

    Web application to take a number input from a user and return a range of numbers from 1 to the chosen number with certain exceptions

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,34 @@
 var pingPong = function (count) {
   var resultArray = [];
 
-  for (var index = 1, index <= count, index++) {
-    if () {
-
+  for (var index = 1; index <= count; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      resultArray.push("pingpong");
     }
-    elseif () {
-
+    else if (index % 3 === 0) {
+      resultArray.push("ping");
     }
-    elseif () {
-
+    else if (index % 5 === 0 ) {
+      resultArray.push("pong");
     }
-    else () {
-      
+    else {
+        resultArray.push(index);
     }
   }
-
+  return resultArray;
 }
 
+function showResult(value) {
+  $(".ppList").append("<li>" + value + "</li>");
+}
 
 //user interface logic
 $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
+    $(".ppList").empty();
     var count = parseInt($("#number").val());
     var resultArray = pingPong(count);
-
-    $("#result").text(resultArray);
+    resultArray.forEach(showResult);
   });
 });
